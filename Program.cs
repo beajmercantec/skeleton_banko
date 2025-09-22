c5d64978b2292de78ca83b3cf4ffd3aa26520a7f
beajmercantec <beaj@mercantec.dk>
2025-09-22 13:49:11 +0200

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+
+namespace Banko
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // TODO: Opret en liste af bankoplader
+            // TODO: Tilføj nye plader til listen
+            // TODO: Marker trukne tal
+            // TODO: Tjek om en plade har banko (1 række, 2 rækker, fuld plade)
+
+            Console.WriteLine("Velkommen til Banko!");
+        }
+    }
+}
